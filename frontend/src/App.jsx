7496cd0f9020401befaf1e33362069bc5b3aea71
Shobi <it21821240@my.sliit.lk>
2025-09-08 23:06:56 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import {Routes, Route} from 'react-router-dom';
 import {Layout} from './components/common/Layout.jsx';
 import {LandingPage} from './pages/LandingPage.jsx';
 import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import RecommendPage from "./pages/RecommandPage.jsx";
+import ChatPage from './pages/ChatPage.jsx';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
   return (
@@ -10,7 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/explore-groups" element={<RecommendPage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
+        {/* Add more routes as needed */}
       </Routes>
+      <ToastContainer position="top-right" autoClose={3500} theme="light" />
     </Layout>
   );
 }
