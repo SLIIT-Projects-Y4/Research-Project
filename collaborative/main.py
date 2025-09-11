18bea8644d302f1e9f290d0c518d47fc0a7681b8
Shobi <it21821240@my.sliit.lk>
2025-09-12 01:04:44 +0530

    add missing routers

--- collaborative/main.py
+++ collaborative/main.py
@@ -3,9 +3,11 @@ from routes.group_routes import router as group
 from routes.recommendation import router as recommend_router
 from routes.group_creator import router as group_creator_router
 from routes.chat_ws import router as chat_ws_router
+from routes.moderation import router as moderation_router
+from routes.translate import router as translate_router
+from routes.polls import router as polls_router
 from database.database import db
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()  # ✅ define app first
 
@@ -18,6 +20,9 @@ app.include_router(group)
 app.include_router(recommend_router)
 app.include_router(group_creator_router)
 app.include_router(chat_ws_router)
+app.include_router(moderation_router)
+app.include_router(translate_router)
+app.include_router(polls_router)
 
 # Allow CORS (cross-origin requests)
 app.add_middleware(
